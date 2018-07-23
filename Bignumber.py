26542b96b5876fda57fa7b00d1e3598bf2624342
0x00ZERO <36645373+AbdullahALRashdan@users.noreply.github.com>
2018-07-23 06:33:52 -0500

    Add files via upload

---
+++ Bignumber.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import requests 
+
+payload = hex(999)
+url="http://ec2-35-164-4-176.us-west-2.compute.amazonaws.com/bignumber/?password="+payload[2:]
+session = requests.Session()
+res = session.get(url)
+print res.text.split("<center><h1>")[1]
